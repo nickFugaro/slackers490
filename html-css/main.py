b594d08a370a7513dda91dff5f6dd1db0e31f4da
speshori <sap229@njit.edu>
2021-02-21 12:47:11 -0500

    Have test api calls passing into movies.html page

--- html-css/main.py
+++ html-css/main.py
@@ -6,12 +6,10 @@ import requests
 app = flask.Flask(__name__)
 
 
-def movieCall():
-    req = requests.get("https://swapi.dev/api/films/1/")
+def movieCall(number):
+    req = requests.get("https://swapi.dev/api/films/" + str(number)+ "/")
     json = req.json()
-    print(json)
     return json
-
 
 
 @app.route('/')
@@ -47,10 +45,20 @@ def login():
 
 @app.route('/movies.html')
 def movies():
-    movie1 = movieCall()
-    print(movie1)
+    movie1 = movieCall(4)
+    movie2 = movieCall(5)
+    movie3 = movieCall(6)
+    movie4 = movieCall(1)
+    movie5 = movieCall(2)
+    movie6 = movieCall(3)
     return flask.render_template(
         "movies.html",
+        movie1info=movie1["opening_crawl"],
+        movie2info=movie2["opening_crawl"],
+        movie3info=movie3["opening_crawl"],
+        movie4info=movie4["opening_crawl"],
+        movie5info=movie5["opening_crawl"],
+        movie6info=movie6["opening_crawl"]
         )
 
 @app.route('/news.html')
