08b79b6eabc9e55c68f1f3fdd9a8893abf751cb6
speshori <sap229@njit.edu>
2021-02-21 12:18:02 -0500

    All pages hosted on flask

--- html-css/main.py
+++ html-css/main.py
@@ -6,16 +6,52 @@ import requests
 app = flask.Flask(__name__)
 
 @app.route('/')
+def indexload():
+    return flask.render_template(
+        "index.html",
+        )
+
+@app.route('/index.html')
 def index():
     return flask.render_template(
         "index.html",
         )
 
 
-@app.route('/forum')
+@app.route('/forum.html')
 def forum():
     return flask.render_template(
         "forum.html",
+        )
+
+@app.route('/about.html')
+def about():
+    return flask.render_template(
+        "about.html",
+        )
+
+@app.route('/login-signup.html')
+def login():
+    return flask.render_template(
+        "login-signup.html",
+        )
+
+@app.route('/movies.html')
+def movies():
+    return flask.render_template(
+        "movies.html",
+        )
+
+@app.route('/news.html')
+def news():
+    return flask.render_template(
+        "news.html",
+        )
+        
+@app.route('/quizzes.html')
+def quizzes():
+    return flask.render_template(
+        "quizzes.html",
         )
 
 app.run(
