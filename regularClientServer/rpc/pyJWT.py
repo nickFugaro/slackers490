6a88200c95c7a868c28d18a0c30ad58a55d57a57
Nick 2 <nff3@njit.edu>
2021-02-21 22:03:45 -0500

    hooked it up to previous stuff

---
+++ regularClientServer/rpc/pyJWT.py
@@ -0,0 +1,32 @@
+
+import jwt
+import datetime
+
+class JWT:
+	
+	secretKey = "Slackers-IT490" #THIS KEY MUST NOT CHANGE AT ANY TIME
+	algo = "HS256"
+	
+	def __init__(self):
+		pass
+
+	def getToken(self,email):
+		issueAt = datetime.datetime.now().timestamp()
+		body = {
+			"iss" : "slackers490.IT490",
+			"iat" : issueAt,
+			"exp" : issueAt+3600,
+			"email" : email
+		}
+		return(jwt.encode(body,self.secretKey,algorithm=self.algo))
+		   
+	def verifyToken(self,token):
+		decodedToken = None
+		try:
+			decodedToken = jwt.decode(token,self.secretKey,algorithm=self.algo)
+		except jwt.ExpiredSignatureError as error:
+			print("RETURN TOKEN EXPIRED")
+			return("TOKEN EXPIRED, Please Signin Again")
+			   
+		print("RETURN: ",decodedToken.get('email'))
+		return(decodedToken.get('email'))
